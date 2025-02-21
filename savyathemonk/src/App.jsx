f6040bd975ce372a4e1ccefbada348dd853e04b9
Kenneth Basjan <155391671+Kenneth-Basjan-Weberu@users.noreply.github.com>
2025-02-21 12:14:17 +0200

    Updated App.jsx

--- savyathemonk/src/App.jsx
+++ savyathemonk/src/App.jsx
@@ -1,38 +1,51 @@
-import { useState } from 'react'
-import { BrowserRouter } from "react-router-dom";
-import { Navbar, Hero, About, Donate, Contact, Footer } from './components';
-import './App.css'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import "./index.css";
+import { useState, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import "./App.css";
+import Menu from "./components/Menu/Menu";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Updates from "./pages/updates/Updates";
+import Solutions from "./pages/solutions/Solutions";
+import Contact from "./pages/contact/Contact";
 
-const App = () => {
-  const [count, setCount] = useState(0)
+import { AnimatePresence } from "framer-motion";
+
+function App() {
+  const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isDarkMenu = location.pathname === "/updates";
+
+  const pageTitles = {
+    "/": "Elevate Your Digital Presence with Weberu",
+    "/about": "About Us | Weberu",
+    "/solutions": "Solutions | Weberu",
+    "/updates": "Updates | Weberu",
+    "/contact": "Contact | Weberu",
+  };
+
+  useEffect(() => {
+    const currentTitle = pageTitles[location.pathname] || "Aiden Brooks";
+    document.title = currentTitle;
+
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 750);
+  }, [location.pathname]);
 
   return (
-    <BrowserRouter>
-    <ToastContainer
-      position="top-right"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="dark"
-    />
-    <div className=''>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Donate/>
-      <Contact/>
-      <Footer/>
+    <div className="app">
+      <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} isDark={isDarkMenu} />
+      <AnimatePresence mode="wait" initial={false}>
+        <Routes location={location} key={location.pathname}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/updates" element={<Updates />} />
+          <Route path="/solutions" element={<Solutions />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </AnimatePresence>
     </div>
-    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
